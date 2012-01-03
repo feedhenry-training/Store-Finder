4042272e87e4edefbb88e39eddceb8d19dfc8839
cianclarke <cian@cianclarke.com>
2012-01-03 15:15:25 +0000

    Hardcoded location

--- client/default/js/controller/locationController.js
+++ client/default/js/controller/locationController.js
@@ -8,34 +8,8 @@
       var error = {
         message: 'Unable to get current location'
       };
-
-      console.log('calling $fh.geo: $fh>' + typeof $fh);
-      console.log('calling $fh.geo: $fh.geo>' + typeof $fh.geo);
-      $fh.geo({}, function (res) {
-        console.log('$fh.geo success');
-        if (res && res.lat && res.lon) {
-          console.log('got $fh.geo res: ' + JSON.stringify(res));
-          var location = {lat: res.lat, lon: res.lon}; 
-          callback(null, location);
-        }
-        else {
-          callback(error);
-        }
-      }, function (code, props, e) {
-        console.log('$fh.geo fail');
-        if (CONFIG.sampleLocation) {
-          console.log('setting timeout');
-          setTimeout(function () {
-            var location = {lat: 52.25, lon: -7.12};
-            console.log('timeout callback:' + location);
-            callback(null, location);          
-          }, CONFIG.fakeServerTimeout);
-        }
-        else {
-          callback(error);
-        }
-      });
-    
+      var location = {lat: '52.253401', lon: '-7.187886'}; 
+      callback(null, location);    
     }
   });
 }());
