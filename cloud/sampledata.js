d02343c9b8861995c784a50546d6058fcb21b66c
Damian Beresford <Damian.Beresford@feedhenry.com>
2012-01-19 16:04:31 +0000

    Split sample data into its own file

---
+++ cloud/sampledata.js
@@ -0,0 +1,661 @@
+exports.sampleResponse = 
+  [
+    {
+      "address":[
+        "18a St Johns Park",
+        "Waterford City",
+        "Waterford City"
+      ],
+      "county":"Co Waterford",
+      "distance":1,
+      "hours":[
+        "07:30 - 21:00",
+        "08:00 - 20:00",
+        "08:00 - 20:00"
+      ],
+      "location":{
+        "lat":52.26,
+        "lon":-7.11
+      },
+      "name":"Johns Shop",
+      "phone":[
+        "051123456",
+        "0861234567"
+      ],
+      "products":[
+        "Dolls",
+        "Trucks",
+        "Cars",
+        "Footballs",
+        "Bikes",
+        "Skateboards",
+        "Lego",
+        "Books",
+        "Cards"
+      ]
+    },
+    {
+      "address":[
+        "Lismore Park",
+        "Waterford City",
+        "Waterford City"
+      ],
+      "county":"Co Waterford",
+      "distance":1,
+      "hours":[
+        "07:00 - 22:00",
+        "07:00 - 22:00",
+        "08:00 - 22:00"
+      ],
+      "location":{
+        "lat":52.25,
+        "lon":-7.14
+      },
+      "name":"Lismore Shop",
+      "phone":[
+        "51374046"
+      ],
+      "products":[
+        "Dolls",
+        "Trucks",
+        "Cars",
+        "Footballs",
+        "Bikes",
+        "Skateboards",
+        "Lego",
+        "Books",
+        "Cards"
+      ]
+    },
+    {
+      "address":[
+        "49 Ballybricken",
+        "Waterford City",
+        "Waterford City"
+      ],
+      "county":"Co Waterford",
+      "distance":1,
+      "hours":[
+        "09:00 - 18:00 Thurs/Fri: 09:00",
+        "09:00 - 18:00",
+        "12:00 - 18:00"
+      ],
+      "location":{
+        "lat":52.26,
+        "lon":-7.12
+      },
+      "name":"Ballybricken Shop",
+      "phone":[
+        "051872536"
+      ],
+      "products":[
+        "Dolls",
+        "Trucks",
+        "Cars",
+        "Footballs",
+        "Bikes",
+        "Lego",
+        "Books",
+        "Cards",
+        "Comics"
+      ]
+    },
+    {
+      "address":[
+        "Ballybeg",
+        "Waterford City",
+        "Waterford Town"
+      ],
+      "county":"Co Waterford",
+      "distance":1,
+      "hours":[
+        "08:00 - 22:00",
+        "08:00 - 22:00",
+        "08:00 - 22:00"
+      ],
+      "location":{
+        "lat":52.26,
+        "lon":-7.12
+      },
+      "name":"Ballybeg Shop",
+      "phone":[
+        "051372449"
+      ],
+      "products":[
+        "Dolls",
+        "Trucks",
+        "Cars",
+        "Footballs",
+        "Bikes",
+        "Skateboards",
+        "Lego",
+        "Books",
+        "Cards"
+      ]
+    },
+    {
+      "address":[
+        "Park Road",
+        "Waterford City",
+        "Waterford City"
+      ],
+      "county":"Co Waterford",
+      "distance":2,
+      "hours":[
+        "07:00 - 21:45",
+        "07:30 - 21:45",
+        "07:30 - 21:45"
+      ],
+      "location":{
+        "lat":52.26,
+        "lon":-7.1
+      },
+      "name":"Park Rd Shop",
+      "phone":[
+        "051852641"
+      ],
+      "products":[
+        "Dolls",
+        "Trucks",
+        "Cars",
+        "Footballs",
+        "Bikes",
+        "Lego",
+        "Books",
+        "Cards",
+        "Comics"
+      ]
+    },
+    {
+      "address":[
+        "42 The Quay",
+        "Waterford City",
+        "Waterford City"
+      ],
+      "county":"Co Waterford",
+      "distance":2,
+      "hours":[
+        "07:00 - 22:00",
+        "08:00 - 22:00",
+        "09:00 - 21:00"
+      ],
+      "location":{
+        "lat":52.24,
+        "lon":-7.11
+      },
+      "name":"Quay Shop",
+      "phone":[
+        "051841626"
+      ],
+      "products":[
+        "Dolls",
+        "Trucks",
+        "Cars",
+        "Footballs",
+        "Bikes",
+        "Skateboards",
+        "Lego",
+        "Books",
+        "Cards"
+      ]
+    },
+    {
+      "address":[
+        "Waterford Regional TC",
+        "Cork Road",
+        "Waterford City"
+      ],
+      "county":"Co Waterford",
+      "distance":3,
+      "hours":[
+        "09:00 - 18:00 Thurs/Fri: 09:00",
+        "09:00 - 18:00",
+        "12:00 - 18:00"
+      ],
+      "location":{
+        "lat":52.24,
+        "lon":-7.16
+      },
+      "name":"College Shop",
+      "phone":[
+        "051302621"
+      ],
+      "products":[
+        "Dolls",
+        "Trucks",
+        "Cars",
+        "Footballs",
+        "Bikes",
+        "Skateboards",
+        "Lego",
+        "Books",
+        "Cards"
+      ]
+    },
+    {
+      "address":[
+        "The Quay",
+        "Waterford City",
+        "Waterford City"
+      ],
+      "county":"Co Waterford",
+      "distance":3,
+      "hours":[
+        "07:30 - 19:30",
+        "07:30 - 19:30",
+        "07:30 - 19:30"
+      ],
+      "location":{
+        "lat":52.24,
+        "lon":-7.08
+      },
+      "name":"Quay Shop 2",
+      "phone":[
+        "051841626",
+        "0861288453"
+      ],
+      "products":[
+        "Dolls",
+        "Trucks",
+        "Cars",
+        "Footballs",
+        "Bikes",
+        "Skateboards",
+        "Lego",
+        "Books",
+        "Cards"
+      ]
+    },
+    {
+      "address":[
+        "Cork Road",
+        "Waterford City",
+        "Waterford City"
+      ],
+      "county":"Co Waterford",
+      "distance":3,
+      "hours":[
+        "07:00 - 23:00",
+        "07:00 - 23:00",
+        "08:00 - 23:00"
+      ],
+      "location":{
+        "lat":52.24,
+        "lon":-7.16
+      },
+      "name":"Village Shop",
+      "phone":[
+        "051854363"
+      ],
+      "products":[
+        "Dolls",
+        "Trucks",
+        "Cars",
+        "Footballs",
+        "Bikes",
+        "Lego",
+        "Books",
+        "Cards",
+        "Comics"
+      ]
+    },
+    {
+      "address":[
+        "Farren Park",
+        "Upper Grange Road",
+        "Waterford City"
+      ],
+      "county":"Co Waterford",
+      "distance":3,
+      "hours":[
+        "08:00 - 22:00",
+        "08:00 - 22:00",
+        "08:00 - 22:00"
+      ],
+      "location":{
+        "lat":52.24,
+        "lon":-7.09
+      },
+      "name":"Farren Shop",
+      "phone":[
+        "51856296"
+      ],
+      "products":[
+        "Dolls",
+        "Trucks",
+        "Cars",
+        "Footballs",
+        "Bikes",
+        "Lego",
+        "Books",
+        "Cards",
+        "Comics"
+      ]
+    },
+    {
+      "address":[
+        "Unit 9 Brasscock Street",
+        "Ballinakill",
+        "Ballinakill"
+      ],
+      "county":"Co Waterford",
+      "distance":4,
+      "hours":[
+        "07:00 - 22:00",
+        "08:00 - 22:00",
+        "08:00 - 22:00"
+      ],
+      "location":{
+        "lat":52.25,
+        "lon":-7.07
+      },
+      "name":"Ballinakill Shop",
+      "phone":[
+        "51820172"
+      ],
+      "products":[
+        "Dolls",
+        "Trucks",
+        "Cars",
+        "Footballs",
+        "Bikes",
+        "Lego",
+        "Books",
+        "Cards",
+        "Comics"
+      ]
+    },
+    {
+      "address":[
+        "Riverstown",
+        "Tramore",
+        "Tramore"
+      ],
+      "county":"Co Waterford",
+      "distance":10,
+      "hours":[
+        "07:30 - 21:30",
+        "07:30 - 21:30",
+        "07:30 - 21:30"
+      ],
+      "location":{
+        "lat":52.16,
+        "lon":-7.14
+      },
+      "name":"Tramore Shop",
+      "phone":[
+        "051390289"
+      ],
+      "products":[
+        "Dolls",
+        "Trucks",
+        "Cars",
+        "Footballs",
+        "Bikes",
+        "Skateboards",
+        "Lego",
+        "Books"
+      ]
+    },
+    {
+      "address":[
+        "Brown Street",
+        "Portlaw",
+        "Portlaw"
+      ],
+      "county":"Co Waterford",
+      "distance":13,
+      "hours":[
+        "07:30 - 21:30",
+        "07:30 - 21:30",
+        "08:30 - 21:30"
+      ],
+      "location":{
+        "lat":52.29,
+        "lon":-7.31
+      },
+      "name":"Kiely's Shop",
+      "phone":[
+        "51387654"
+      ],
+      "products":[
+        "Dolls",
+        "Trucks",
+        "Cars",
+        "Footballs",
+        "Bikes",
+        "Lego",
+        "Books",
+        "Cards",
+        "Comics"
+      ]
+    },
+    {
+      "address":[
+        "Bridge House",
+        "Portlaw",
+        "Portlaw"
+      ],
+      "county":"Co Waterford",
+      "distance":13,
+      "hours":[
+        "07:00 - 18:00",
+        "08:00 - 18:00",
+        "08:00 - 13:00"
+      ],
+      "location":{
+        "lat":52.29,
+        "lon":-7.31
+      },
+      "name":"Fogarty's Shop",
+      "phone":[
+        "51387061"
+      ],
+      "products":[
+        "Dolls",
+        "Trucks",
+        "Cars",
+        "Footballs",
+        "Bikes",
+        "Skateboards",
+        "Lego",
+        "Books"
+      ]
+    },
+    {
+      "address":[
+        "Unit 4 Dock Road",
+        "Dunmore East",
+        "Dunmore East"
+      ],
+      "county":"Co Waterford",
+      "distance":15,
+      "hours":[
+        "07:30 - 20:00",
+        "07:30 - 20:00",
+        "07:30 - 20:00"
+      ],
+      "location":{
+        "lat":52.15,
+        "lon":-6.99
+      },
+      "name":"Dingley's",
+      "phone":[
+        "051383372"
+      ],
+      "products":[
+        "Dolls",
+        "Trucks",
+        "Cars",
+        "Footballs",
+        "Bikes",
+        "Lego",
+        "Books",
+        "Cards",
+        "Comics"
+      ]
+    },   
+    {
+      "address":[
+        "The Quay",
+        "Dungarvan",
+        "Dungarvan"
+      ],
+      "county":"Co Waterford",
+      "distance":38,
+      "hours":[
+        "08:00 - 21:00",
+        "08:00 - 21:00",
+        "08:00 - 20:00"
+      ],
+      "location":{
+        "lat":52.09,
+        "lon":-7.62
+      },
+      "name":"SuperValu Dungarvan",
+      "phone":[
+        "05841754"
+      ],
+      "products":[
+        "Dolls",
+        "Trucks",
+        "Cars",
+        "Footballs",
+        "Bikes",
+        "Skateboards",
+        "Lego",
+        "Books",
+        "Cards"
+      ]
+    },
+    {
+      "address":[
+        "Clodagh Road",
+        "Dungarvan",
+        "Dungarvan"
+      ],
+      "county":"Co Waterford",
+      "distance":39,
+      "hours":[
+        "08:00 - 22:00",
+        "08:00 - 22:00",
+        "08:00 - 22:00"
+      ],
+      "location":{
+        "lat":52.09,
+        "lon":-7.63
+      },
+      "name":"Centra Dungarvan",
+      "phone":[
+        ""
+      ],
+      "products":[
+        "Dolls",
+        "Trucks",
+        "Cars",
+        "Footballs",
+        "Bikes",
+        "Skateboards",
+        "Lego",
+        "Books",
+        "Cards"
+      ]
+    },
+    {
+      "address":[
+        "Ballyrafter",
+        "Lismore",
+        "Lismore"
+      ],
+      "county":"Co Waterford",
+      "distance":56,
+      "hours":[
+        "07:30 - 21:30",
+        "09:00 - 21:00",
+        "09:00 - 21:00"
+      ],
+      "location":{
+        "lat":52.14,
+        "lon":-7.93
+      },
+      "name":"Mace Ballyrafter",
+      "phone":[
+        "05854665"
+      ],
+      "products":[
+        "Dolls",
+        "Trucks",
+        "Cars",
+        "Footballs",
+        "Bikes",
+        "Lego",
+        "Books",
+        "Cards",
+        "Comics"
+      ]
+    },
+    {
+      "address":[
+        "The Wine Vaults",
+        "Lismore",
+        "Lismore"
+      ],
+      "county":"Co Waterford",
+      "distance":56,
+      "hours":[
+        "08:00 - 21:00",
+        "08:00 - 21:00",
+        "08:00 - 20:00"
+      ],
+      "location":{
+        "lat":52.14,
+        "lon":-7.93
+      },
+      "name":"Londis Lismore",
+      "phone":[
+        "05854279"
+      ],
+      "products":[
+        "Dolls",
+        "Trucks",
+        "Bikes",
+        "Lego",
+        "Books",
+        "Cards",
+        "Comics"
+      ]
+    },
+    {
+      "address":[
+        "Main Street",
+        "Ballyduff",
+        "Ballyduff"
+      ],
+      "county":"Co Waterford",
+      "distance":65,
+      "hours":[
+        "09:00 - 20:00 Fri: 09:00 - 21:",
+        "09:00 - 20:00",
+        "09:00 - 13:00"
+      ],
+      "location":{
+        "lat":52.15,
+        "lon":-8.06
+      },
+      "name":"Mace Ballyduff",
+      "phone":[
+        "0667131130"
+      ],
+      "products":[
+        "Dolls",
+        "Trucks",
+        "Cars",
+        "Footballs",
+        "Bikes",
+        "Lego",
+        "Books",
+        "Cards",
+        "Comics"
+      ]
+    }
+];
